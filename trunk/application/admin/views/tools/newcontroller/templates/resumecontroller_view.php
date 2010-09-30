e911bd88f85effc4081d1930fe28d27644a2ba31
flamenjaut@gmail.com <flamenjaut@gmail.com@57fbed19-85c0-1a23-a79c-c28bc9e248a6>
2010-09-30 01:46:35 +0000

    New form wizard: 	- Pagination included by default in datagrids

--- trunk/application/admin/views/tools/newcontroller/templates/resumecontroller_view.php
+++ trunk/application/admin/views/tools/newcontroller/templates/resumecontroller_view.php
@@ -5,21 +5,34 @@ class <?php echo $classname;?> extends Controller {
 
 		parent::Controller();
 		$this->load->helper('url');
+		$this->load->library('pagination'); 
 	}
 
-	function index() {
+	function index($current_page = 0) {
 
 		$this->load->database();
-		
-		<?php if(isset($where)) { echo '$where = '.$where."\r\n"; } ?>
-		$content = $this->db->f_select(array('<?php echo implode("','",$dbs);?>'),
-		'<?php echo implode(", ",$data);?>'<?php if(isset($where)) { echo ',$where'; } ?>)->result();
 
+		/*** get records ***/
+		<?php if(isset($where)) { echo '$where = '.$where."\r\n"; } ?>
+		$extra = "limit $current_page,20";
+		$content = $this->db->f_select(array('<?php echo implode("','",$dbs);?>'),
+		'<?php echo implode(", ",$data);?>'<?php if(isset($where)) { echo ',$where'; } ?>,$extra)->result();
+
+		/*** set pagination ***/
+		$config['base_url'] = site_url("<?php echo $vpath;?>");
+		$config['total_rows'] = $this->db->f_select(array('<?php echo implode("','",$dbs);?>'),'count(*) as itemNum'<?php if(isset($where)) { echo ',$where'; } ?>,"limit 20")->row()->itemNum;
+		$config['per_page'] = '20';
+		$config['uri_segment'] = <?php echo substr_count($vpath, "/") + 2; ?>;
+
+		$this->pagination->initialize($config); 
+
+		/*** load view ***/
 		$data = array(
 <?php foreach($assets as $key => $val) {
 			echo "\t\t\t'".$key."' => array(".$val."),\r\n";
  } ?>
-			'content'=>$content
+			'content' => $content,
+			'pagination' => $this->pagination->create_links() 
 		); 
 		$this->load->masterview('<?php echo $vpath;?>/datagrid', $data, "default");
 	}
