48ae478b542ba72a129b71c2e4aef227916cf1f0
flamenjaut <flamenjaut@gmail.com>
2010-11-05 00:04:08 +0000

    Ajax library: Added submitButton method. Removed masterview dependency

--- libraries/Output.php
+++ libraries/Output.php
@@ -1,160 +1,17 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * CodeIgniter
+ * F-engine
  *
- * An open source application development framework for PHP 4.3.2 or newer
- *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008 - 2009, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * @package		F-engine
+ * @author		flmn
+ * @copyright	Copyright (c) 2010, Mikel Madariaga
+ * @license		http://www.f-engine.net/userguide/license
+ * @link		http://www.f-engine.net/
+ * @since		Version 0.5
  * @filesource
  */
-
-// ------------------------------------------------------------------------
-
-/**
- * Output Class
- *
- * Responsible for sending final output to browser
- *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Output
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/libraries/output.html
- */
-class CI_Output {
-
-	var $final_output;
-	var $cache_expiration	= 0;
-	var $headers 			= array();
-	var $enable_profiler 	= FALSE;
-
-
-	function CI_Output()
-	{
-		log_message('debug', "Output Class Initialized");
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Get Output
-	 *
-	 * Returns the current output string
-	 *
-	 * @access	public
-	 * @return	string
-	 */	
-	function get_output()
-	{
-		return $this->final_output;
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Set Output
-	 *
-	 * Sets the output string
-	 *
-	 * @access	public
-	 * @param	string
-	 * @return	void
-	 */	
-	function set_output($output)
-	{
-		$this->final_output = $output;
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	 * Append Output
-	 *
-	 * Appends data onto the output string
-	 *
-	 * @access	public
-	 * @param	string
-	 * @return	void
-	 */	
-	function append_output($output)
-	{
-		if ($this->final_output == '')
-		{
-			$this->final_output = $output;
-		}
-		else
-		{
-			$this->final_output .= $output;
-		}
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	 * Set Header
-	 *
-	 * Lets you set a server header which will be outputted with the final display.
-	 *
-	 * Note:  If a file is cached, headers will not be sent.  We need to figure out
-	 * how to permit header data to be saved with the cache data...
-	 *
-	 * @access	public
-	 * @param	string
-	 * @return	void
-	 */	
-	function set_header($header, $replace = TRUE)
-	{
-		$this->headers[] = array($header, $replace);
-	}
-
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Set HTTP Status Header
-	 * moved to Common procedural functions in 1.7.2
-	 * 
-	 * @access	public
-	 * @param	int 	the status code
-	 * @param	string	
-	 * @return	void
-	 */	
-	function set_status_header($code = '200', $text = '')
-	{
-		set_status_header($code, $text);
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Enable/disable Profiler
-	 *
-	 * @access	public
-	 * @param	bool
-	 * @return	void
-	 */	
-	function enable_profiler($val = TRUE)
-	{
-		$this->enable_profiler = (is_bool($val)) ? $val : TRUE;
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Set Cache
-	 *
-	 * @access	public
-	 * @param	integer
-	 * @return	void
-	 */	
-	function cache($time)
-	{
-		$this->cache_expiration = ( ! is_numeric($time)) ? 0 : $time;
-	}
+require('CI_Output.php');
+class CI_Output extends Output {
 	
 	// --------------------------------------------------------------------
 	
@@ -260,12 +117,34 @@ class CI_Output {
 			if (preg_match("|</body>.*?</html>|is", $output))
 			{
 				$output  = preg_replace("|</body>.*?</html>|is", '', $output);
+				if(!defined('MASTERVIEW') and isset($CI->ajax)) {
+					$output .= $CI->ajax->getString(true);
+				}
 				$output .= $CI->profiler->run();
 				$output .= '</body></html>';
 			}
 			else
 			{
+				if(!defined('MASTERVIEW') and isset($CI->ajax)) {
+					$output .= $CI->ajax->getString(true);
+				}
 				$output .= $CI->profiler->run();
+			}
+			
+		} else {
+		
+			if(!defined('MASTERVIEW') and isset($CI->ajax)) {
+
+				if (preg_match("|</body>.*?</html>|is", $output))
+				{
+					$output  = preg_replace("|</body>.*?</html>|is", '', $output);
+					$output .= $CI->ajax->getString(true);
+					$output .= '</body></html>';
+				}
+				else
+				{
+					$output .= $CI->ajax->getString(true);
+				}
 			}
 		}
 		
@@ -286,130 +165,4 @@ class CI_Output {
 		log_message('debug', "Total execution time: ".$elapsed);		
 	}
 	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Write a Cache File
-	 *
-	 * @access	public
-	 * @return	void
-	 */	
-	function _write_cache($output)
-	{
-		$CI =& get_instance();	
-		$path = $CI->config->item('cache_path');
-	
-		$cache_path = ($path == '') ? BASEPATH.'cache/fullpage/'.APPNAME : $path;
-
-		if ( ! is_dir($cache_path))
-		{
-			mkdir($cache_path, 0777, true);
-			chmod($cache_path, 0777);
-		}
-		
-		if ( ! is_dir($cache_path) OR ! is_really_writable($cache_path))
-		{
-			return;
-		}
-		
-		$uri =	$CI->config->item('base_url').
-				$CI->config->item('index_page').
-				$CI->uri->uri_string();
-		
-		$cache_path .= md5($uri);
-
-		if ( ! $fp = @fopen($cache_path, FOPEN_WRITE_CREATE_DESTRUCTIVE))
-		{
-			log_message('error', "Unable to write cache file: ".$cache_path);
-			return;
-		}
-		
-		$expire = time() + ($this->cache_expiration * 60);
-		
-		if (flock($fp, LOCK_EX))
-		{
-			fwrite($fp, $expire.'TS--->'.$output);
-			flock($fp, LOCK_UN);
-		}
-		else
-		{
-			log_message('error', "Unable to secure a file lock for file at: ".$cache_path);
-			return;
-		}
-		fclose($fp);
-		@chmod($cache_path, DIR_WRITE_MODE);
-
-		log_message('debug', "Cache file written: ".$cache_path);
-	}
-
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Update/serve a cached file
-	 *
-	 * @access	public
-	 * @return	void
-	 */	
-	function _display_cache(&$CFG, &$URI)
-	{
-		$cache_path = ($CFG->item('cache_path') == '') ? BASEPATH.'cache/' : $CFG->item('cache_path');
-			
-		if ( ! is_dir($cache_path) OR ! is_really_writable($cache_path))
-		{
-			return FALSE;
-		}
-		
-		// Build the file path.  The file name is an MD5 hash of the full URI
-		$uri =	$CFG->item('base_url').
-				$CFG->item('index_page').
-				$URI->uri_string;
-				
-		$filepath = $cache_path.md5($uri);
-		
-		if ( ! @file_exists($filepath))
-		{
-			return FALSE;
-		}
-	
-		if ( ! $fp = @fopen($filepath, FOPEN_READ))
-		{
-			return FALSE;
-		}
-			
-		flock($fp, LOCK_SH);
-		
-		$cache = '';
-		if (filesize($filepath) > 0)
-		{
-			$cache = fread($fp, filesize($filepath));
-		}
-	
-		flock($fp, LOCK_UN);
-		fclose($fp);
-					
-		// Strip out the embedded timestamp		
-		if ( ! preg_match("/(\d+TS--->)/", $cache, $match))
-		{
-			return FALSE;
-		}
-		
-		// Has the file expired? If so we'll delete it.
-		if (time() >= trim(str_replace('TS--->', '', $match['1'])))
-		{ 		
-			@unlink($filepath);
-			log_message('debug', "Cache file has expired. File deleted");
-			return FALSE;
-		}
-
-		// Display the cache
-		$this->_display(str_replace($match['0'], '', $cache));
-		log_message('debug', "Cache file is current. Sending it to browser.");		
-		return TRUE;
-	}
-
-
 }
-// END Output Class
-
-/* End of file Output.php */
-/* Location: ./system/libraries/Output.php */
